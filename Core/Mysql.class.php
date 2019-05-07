d156ef4eacc0a1f2a06dbd0fa52c7c8af9b67942
子明 <37722993+zimings@users.noreply.github.com>
2019-05-07 09:46:16 +0800

    Update Mysql.class.php

--- Core/Mysql.class.php
+++ Core/Mysql.class.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * 数据库查询封装
  */
 
@@ -19,11 +19,11 @@ class Mysql
         self::$mysqli = $this->connect($config);
     }
 
+    /**
+     * 连接数据库函数
+     */
     private function connect($config)
     {
-        /**
-         * 连接数据库函数
-         **/
         $mysqli = new mysqli($config['host'], $config['username'], $config['password'], $config['dbname']);
         if ($mysqli->connect_errno) { //连接成功errno应该为0
             die('Connect Error:' . $mysqli->connect_error);
@@ -32,14 +32,14 @@ class Mysql
         return $mysqli;
     }
 
+    /**
+     * 返回数据
+     *
+     * @param string $sql sql语句
+     * @return bool
+     */
     private function returnData(string $sql): bool
     {
-        /**
-         * 返回数据
-         *
-         * @param string $sql sql语句
-         * @return bool
-         **/
         $result = self::$mysqli->query($sql);
         self::$mysqli->close();
         if ($result) {
@@ -51,15 +51,15 @@ class Mysql
         }
     }
 
+    /**
+     * 对where进行处理
+     *
+     * @param array $where 过滤条件
+     * @param string $connect 连接符
+     * @return string
+     */
     private function jointWhere(array $where = null, string $connect = ' = '): string
     {
-        /**
-         * 对where进行处理
-         *
-         * @param array $where 过滤条件
-         * @param string $connect 连接符
-         * @return string
-         **/
         if (empty($where)) return '';
         $len = 0;
         foreach ($where as $key => $value) {
@@ -80,61 +80,61 @@ class Mysql
         return $where;
     }
 
+    /**
+     * 查询一条数据
+     *
+     * @param mixed $data 查询内容
+     * @param string $table 数据表
+     * @param array $where 过滤条件
+     * @return array
+     */
     public function getOneRow($data, string $table, array $where)
     {
-        /**
-         * 查询一条数据
-         *
-         * @param mixed $data 查询内容
-         * @param string $table 数据表
-         * @param array $where 过滤条件
-         * @return array
-         */
         $result = $this->select($data, $table, $where, 0, 1);
         return $result[0];
     }
 
+    /**
+     * 查询一条数据
+     *
+     * @param mixed $data 查询内容
+     * @param string $table 数据表
+     * @param array $where 过滤条件
+     * @return array
+     */
     public function getRows($data, string $table, array $where = null, int $offset = 0, int $limit = 1000)
     {
-        /**
-         * 查询一条数据
-         *
-         * @param mixed $data 查询内容
-         * @param string $table 数据表
-         * @param array $where 过滤条件
-         * @return array
-         */
         $result = $this->select($data, $table, $where, $offset, $limit);
         return $result;
     }
 
+    /**
+     * 搜索数据
+     *
+     * @param mixed $data 搜索内容
+     * @param string $table 数据表
+     * @param array $where 过滤条件
+     * @return array
+     */
     public function search($data, string $table, array $where)
     {
-        /**
-         * 搜索数据
-         *
-         * @param mixed $data 搜索内容
-         * @param string $table 数据表
-         * @param array $where 过滤条件
-         * @return array
-         */
         $result = $this->select($data, $table, $where, 0, 1000, ' like ');
         return $result;
     }
 
+    /**
+     * 查询数据
+     *
+     * @param mixed $data 查询内容
+     * @param string $table 数据表
+     * @param array $where 过滤条件
+     * @param int $offset 偏移量
+     * @param int $limit 行数限制
+     * @param string $connectStr 限定条件连接符
+     * @return array
+     */
     private function select($data, string $table, array $where = null, int $offset = 0, int $limit = 1000, string $connectStr = ' = ')
     {
-        /**
-         * 查询数据
-         *
-         * @param mixed $data 查询内容
-         * @param string $table 数据表
-         * @param array $where 过滤条件
-         * @param int $offset 偏移量
-         * @param int $limit 行数限制
-         * @param string $connectStr 限定条件连接符
-         * @return array
-         */
         $table = self::$mysqli->real_escape_string($table);
         $where = $this->jointWhere($where, $connectStr);
         if (is_array($data)) {//判断传入数据是否为数组
@@ -154,15 +154,15 @@ class Mysql
         return $result;
     }
 
+    /**
+     * 插入数据
+     *
+     * @param string $table 数据表
+     * @param array $data 数据数组
+     * @return bool
+     */
     public function insert(string $table, array $data): bool
     {
-        /**
-         * 插入数据
-         *
-         * @param string $table 数据表
-         * @param array $data 数据数组
-         * @return bool
-         */
         $table = self::$mysqli->real_escape_string($table);
         foreach ($data as $key => $value) {
             $data[$key] = self::$mysqli->real_escape_string($value);
@@ -173,16 +173,16 @@ class Mysql
         return $this->returnData($sql);
     }
 
+    /**
+     * 更新数据
+     *
+     * @param string $table 数据表
+     * @param array $data 数据数组
+     * @param array $where 过滤条件
+     * @return bool
+     */
     public function update(string $table, array $data, array $where): bool
     {
-        /**
-         * 更新数据
-         *
-         * @param string $table 数据表
-         * @param array $data 数据数组
-         * @param array $where 过滤条件
-         * @return bool
-         */
         $table = self::$mysqli->real_escape_string($table);
         $where = $this->jointWhere($where);
         foreach ($data as $key => $value) {
@@ -199,15 +199,15 @@ class Mysql
         return $this->returnData($sql);
     }
 
+    /**
+     * 删除数据
+     *
+     * @param string $table 数据表
+     * @param array $where 过滤条件
+     * @return bool
+     */
     public function delete(string $table, array $where): bool
     {
-        /**
-         * 删除数据
-         *
-         * @param string $table 数据表
-         * @param array $where 过滤条件
-         * @return bool
-         */
         $where = $this->jointWhere($where);
         $table = self::$mysqli->real_escape_string($table);
         $sql = "DELETE FROM {$table} WHERE {$where}";
